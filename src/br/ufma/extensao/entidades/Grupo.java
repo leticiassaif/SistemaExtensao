dc4e9c7d34113747334b42dca00577986c73eb6c
CeciliaXav1er <mvalexavier@gmail.com>
2026-04-22 01:12:46 -0300

    Ultimas atualizacoes (?)

--- src/br/ufma/extensao/entidades/Grupo.java
+++ src/br/ufma/extensao/entidades/Grupo.java
@@ -2,96 +2,47 @@ package br.ufma.extensao.entidades;
 
 import br.ufma.extensao.enums.StatusGrupo;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static br.ufma.extensao.enums.StatusGrupo.ATIVO;
-
 public class Grupo {
     private Long id;
     private String nome;
-    private String email;
     private String descricao;
-    private Docente responsavel;
+    private String email;
+    private Long docenteResponsavelId;
     private StatusGrupo status;
 
-    // Construtor
-    public Grupo(Long id, String nome, String email, String descricao, Docente responsavel) {
+    public Grupo(Long id, String nome, String descricao, String email, Long docenteResponsavelId) {
         this.id = id;
         this.nome = nome;
-        this.email = email;
         this.descricao = descricao;
-        this.responsavel = responsavel;
-        this.status = ATIVO;
-    }
-
-    //Getters
-    public String getNome() {
-        return nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public Docente getResponsavel() {
-        return responsavel;
-    }
-
-    public StatusGrupo getStatus() {
-        return status;
-    }
-
-    public List<Usuario> getMembros() {
-        return membros;
-    }
-
-    //Setters
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
+        this.docenteResponsavelId = docenteResponsavelId;
+        this.status = StatusGrupo.ATIVO;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+    // Getters
+    public Long getId() { return id; }
+    public String getNome() { return nome; }
+    public String getDescricao() { return descricao; }
+    public String getEmail() { return email; }
+    public Long getDocenteResponsavelId() { return docenteResponsavelId; }
+    public StatusGrupo getStatus() { return status; }
 
-    public void setResponsavel(Docente responsavel) {
-        this.responsavel = responsavel;
-    }
-
-    public void setStatus(StatusGrupo status) {
-        this.status = status;
-    }
-
-    public void setMembros(List<Usuario> membros) {
-        this.membros = membros;
-    }
+    // Setters
+    public void setNome(String nome) { this.nome = nome; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public void setEmail(String email) { this.email = email; }
+    public void setDocenteResponsavelId(Long docenteResponsavelId) { this.docenteResponsavelId = docenteResponsavelId; }
+    public void setStatus(StatusGrupo status) { this.status = status; }
 
     @Override
     public String toString() {
         return "Grupo{" +
-                "nome='" + nome + '\'' +
-                ", tipo='" + tipo + '\'' +
-                ", responsavel=" + responsavel.getNome() +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", email='" + email + '\'' +
+                ", docenteResponsavelId=" + docenteResponsavelId +
                 ", status=" + status +
-                ", membros=" + membros.size() +
                 '}';
     }
 }

4a095ba47ed7b7517a84334736cd1c1543ac8992
Roberta <robertasalomao06@gmail.com>
2026-04-22 02:24:39 -0300

    fim oportunidade (dessa vez acho que deu certo)

--- src/service/OportunidadeService.java
+++ src/service/OportunidadeService.java
@@ -8,6 +8,15 @@ import model.Docente;
 import model.Oportunidade;
 import model.Usuario;
 
+import br.ufma.extensao.enums.Modalidade;
+import br.ufma.extensao.enums.Papel;
+import br.ufma.extensao.enums.Cargo;
+import br.ufma.extensao.enums.StatusOportunidade;
+import br.ufma.extensao.enums.TipoOportunidade;
+import br.ufma.extensao.entidades.Docente;
+import br.ufma.extensao.entidades.Oportunidade;
+import br.ufma.extensao.entidades.Usuario;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +26,8 @@ public class OportunidadeService {
 
 
     public Oportunidade criarOportunidade(String titulo, String descricao, TipoOportunidade tipo, Modalidade modalidade, int cargaHoraria, int vagas, Docente responsavelId, Usuario autor, LocalDate inicio, LocalDate fim){
-        if (autor.getPapel() == Papel.DOCENTE || autor.getPapel() == Papel.DISCENTE_DIRETOR) {
-            Oportunidade oportunidade = new Oportunidade(id, titulo, descricao, tipo, modalidade, cargaHoraria, vagas, StatusOportunidade.RASCUNHO, inicio, fim, autor, responsavelId);
+        if (autor.getPapel() == Papel.DOCENTE || autor.getCargo() == Cargo.DIRETOR) {
+            Oportunidade oportunidade = new Oportunidade( titulo, descricao, tipo, modalidade, cargaHoraria, vagas, StatusOportunidade.RASCUNHO, inicio, fim, autor, responsavelId);
             oportunidades.add(oportunidade);
             return oportunidade;
         }
@@ -28,7 +37,7 @@ public class OportunidadeService {
     public Oportunidade publicarOportunidade(Long id, Usuario autor){
         for (Oportunidade op : oportunidades){
             if (op.getId().equals(id)) {
-                if (autor.getPapel() == Papel.DISCENTE_DIRETOR) {
+                if (autor.getCargo() == Cargo.DIRETOR) {
                     op.setStatus(StatusOportunidade.AGUARDANDO_APROVACAO);
                     return op;
                 }

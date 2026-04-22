f6c3e4df1fd131d0d3d07092ac69f62e18c549a3
Roberta <robertasalomao06@gmail.com>
2026-04-22 13:11:32 -0300

    aproveitamentos

--- src/br/ufma/extensao/servicos/AproveitamentoService.java
+++ src/br/ufma/extensao/servicos/AproveitamentoService.java
@@ -1,6 +1,10 @@
 package br.ufma.extensao.servicos;
 
 import br.ufma.extensao.entidades.Aproveitamento;
+import br.ufma.extensao.entidades.Inscricao;
+import br.ufma.extensao.entidades.Usuario;
+import br.ufma.extensao.enums.Papel;
+import br.ufma.extensao.enums.StatusAproveitamento;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -10,22 +14,66 @@ public class AproveitamentoService {
     private List<Aproveitamento> aproveitamentos = new ArrayList<>();
     private int proximoId = 1;
 
-    public Aproveitamento submeter(String discenteId, String descricao, double cargaHoraria){
-        if (discenteId == null || descricao == null || cargaHoraria <= 0)
+    public Aproveitamento submeter(String discenteId, String descricao, double cargaHorariaPleiteada){
+        if (discenteId == null || descricao == null || cargaHorariaPleiteada <= 0)
             throw new IllegalArgumentException("Dados do aproveitamento são inválidos!");
         String id = ("APR00" + proximoId);
         proximoId++;
-        Aproveitamento aproveitamento = new Aproveitamento(id, discenteId, descricao, cargaHoraria);
+        Aproveitamento aproveitamento = new Aproveitamento(id, discenteId, descricao, cargaHorariaPleiteada);
+        aproveitamentos.add(aproveitamento);
         return aproveitamento;
     }
 
-    public Aproveitamento aprovar(){}
+    public Aproveitamento aprovar(String id, Usuario u, double cargaPleiteada){
+        if (UsuarioService.hasPermissao(u, Papel.COORDENADOR)){
+            for (Aproveitamento apr : aproveitamentos){
+                if (apr.getId().equals(id) || apr.getStatus() == StatusAproveitamento.PENDENTE){
+                    apr.setCargaHorariaAprovada(cargaPleiteada);
+                    apr.setStatus(StatusAproveitamento.APROVADO);
+                }
+            }
+        }
+        throw new IllegalArgumentException("Usuário sem permissão para essa ação!");
+    }
 
-    public Aproveitamento indeferir(){}
+    public Aproveitamento indeferir(String id, Usuario u){
+        if (UsuarioService.hasPermissao(u, Papel.COORDENADOR)){
+            for (Aproveitamento apr : aproveitamentos){
+                if (apr.getId().equals(id) || apr.getStatus() == StatusAproveitamento.PENDENTE){
+                    apr.setStatus(StatusAproveitamento.INDEFERIDO);
+                }
+            }
+        }
+        throw new IllegalArgumentException("Usuário sem permissão para essa ação!");
+    }
 
-    public Aproveitamento reenviar(){}
+    public Aproveitamento reenviar(String id){
+            for (Aproveitamento apr : aproveitamentos){
+                if (apr.getId().equals(id) || apr.getStatus() == StatusAproveitamento.INDEFERIDO){
+                    apr.setStatus(StatusAproveitamento.PENDENTE);
+                }
+            }
+        throw new IllegalArgumentException("Impossível realizar ação!");
+    }
 
-    public List <Aproveitamento> (){}
+    
 
-    public List <Aproveitamento> (){}
+    public List <Aproveitamento> listarPendentes(){
+        List <Aproveitamento> resultado = new ArrayList<>();
+        for (Aproveitamento apr : aproveitamentos) {
+            if (apr.getStatus() == StatusAproveitamento.PENDENTE)
+                resultado.add(apr);
+        }
+        return resultado;
+    }
+
+    public List <Aproveitamento> listarPorDiscente(){
+        List<Aproveitamento> resultado = new ArrayList<>();
+        for (Aproveitamento apr : aproveitamentos){
+            if(apr.getDiscenteId().equals(apr.getId())){
+                resultado.add(apr);
+            }
+        }
+        return resultado;
+    }
 }
